831abaa219e14aa7caca56720c1a932b9dd1b8c5
tuc0w <andreasbehrend@gmail.com>
2019-11-08 23:35:16 +0100

    Fix missing coding standards

--- src/Tuc0wTimeularPublicApiBundle.php
+++ src/Tuc0wTimeularPublicApiBundle.php
@@ -5,11 +5,13 @@ namespace Tuc0w\TimeularPublicApiBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * Class Tuc0wTimeularPublicApiBundle
- * 
+ * Class Tuc0wTimeularPublicApiBundle.
+ *
  * @package Tuc0w\TimeularPublicApiBundle
+ *
  * @author Andreas "tuc0w" Behrend <andreasbehrend@gmail.com>
  */
-class Tuc0wTimeularPublicApiBundle extends Bundle {
+class Tuc0wTimeularPublicApiBundle extends Bundle
+{
     // no logic needed..
 }
